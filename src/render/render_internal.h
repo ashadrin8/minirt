d20caedae5f9dd73152d9ace9190b80d48369ffe
Chiara Kappe <chiarakappe@Chiaras-MacBook-Pro-2.local>
2026-01-17 00:25:58 +0100

    includes & utils

---
+++ src/render/render_internal.h
@@ -0,0 +1,14 @@
+#ifndef RENDER_INTERNAL_H
+# define RENDER_INTERNAL_H
+
+# include "defines.h"
+
+int32_t		create_window(t_scene *scene);
+void		render_scene(t_scene *scene, mlx_image_t *img);
+
+// utils
+uint32_t	rgba(int r, int g, int b, int a);
+double 		dot(t_coordinates a, t_coordinates b);
+
+
+#endif
